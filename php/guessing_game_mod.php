db7b5104033ad916323c2b31101919e5cff674d4
Aleksandr Kostitsyn <kostitsin.a@mail.ru>
2019-12-24 21:38:44 +0400

    Добавил сайт в репозиторий

---
+++ php/guessing_game_mod.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Личный сайт студента GeekBrains</title>
+	<link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+
+<div class="content">
+<?php
+	include "menu.php";
+?>
+
+
+	<div class="center">
+	<img src="img/I.jpg">
+		<div class="box_text">
+		
+		</div>
+	</div>
+</div>
+<div class="footer">
+	Copyright &copy; Vasily Pupkin
+<div>
+
+
+</body>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,7 @@
+	<div class="header">
+		<a href="index.php">Главная</a>
+		<a href="puzzle.html">Загадки</a>
+		<a href="guess.html">Угадайка</a>
+		<a href="guessing_game_mod.html">Угадайка для двух игроков</a>
+		<a href="generate_pass.html">Генератор паролей</a>
+	</div>
